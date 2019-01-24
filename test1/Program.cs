225e1e0c4570d74d2d3370c538c9623bc745cfef
izemo1076 <izemo1076@gmail.com>
2019-01-24 10:03:45 -0700

    First checkin
    First checkin

---
+++ test1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hellow orld derp!");
+            Console.Beep();
+            Console.ReadLine();
+
+        }
+    }
+}
